f3f3665d8102492b42fc63c827a1a70e98ac56a8
Aj Steinbacher <66484594+AjaxCodes@users.noreply.github.com>
2021-05-04 21:29:26 -0500

    sidebar esthetic complete

--- glasstube/src/Components/Sidebar.js
+++ glasstube/src/Components/Sidebar.js
@@ -10,23 +10,23 @@ import HistorySharpIcon from '@material-ui/icons/HistorySharp';
 import OndemandVideoSharpIcon from '@material-ui/icons/OndemandVideoSharp';
 import WatchLaterSharpIcon from '@material-ui/icons/WatchLaterSharp';
 import FavoriteSharpIcon from '@material-ui/icons/FavoriteSharp';
-import KeyboardArrowDownSharpIcon from '@material-ui/icons/KeyboardArrowDownSharp';
+import GetAppSharpIcon from '@material-ui/icons/GetAppSharp';
 
 
 
 function Sidebar() {
   return (
     <div className="sidebar">
-      <SidebarRow Icon={HomeSharpIcon} title="Home" />
-      <SidebarRow Icon={WhatshotSharpIcon} title="Trending"/>
-      <SidebarRow Icon={SubscriptionsSharpIcon}  title="Subscription" />
+      <SidebarRow  Icon={HomeSharpIcon} title="Home" />
+      <SidebarRow  Icon={WhatshotSharpIcon} title="Trending"/>
+      <SidebarRow selected Icon={SubscriptionsSharpIcon}  title="Subscription" />
      <hr/>
-      <SidebarRow Icon={VideoLibrarySharpIcon}  title="Library"/>
-      <SidebarRow Icon={HistorySharpIcon}  title="History"/>
-      <SidebarRow Icon={OndemandVideoSharpIcon}  title="Your Videos"/>
-      <SidebarRow Icon={WatchLaterSharpIcon}  title="Watch Later"/>
-      <SidebarRow Icon={FavoriteSharpIcon}  title="Liked Videos"/>
-      <SidebarRow Icon={KeyboardArrowDownSharpIcon}  title="Show More"/>
+      <SidebarRow  Icon={VideoLibrarySharpIcon}  title="Library"/>
+      <SidebarRow  Icon={HistorySharpIcon}  title="History"/>
+      <SidebarRow  Icon={OndemandVideoSharpIcon}  title="Your Videos"/>
+      <SidebarRow  Icon={WatchLaterSharpIcon}  title="Watch Later"/>
+      <SidebarRow  Icon={FavoriteSharpIcon}  title="Liked Videos"/>
+      <SidebarRow  Icon={GetAppSharpIcon}  title="Show More"/>
       <hr/>
     </div>
   );
